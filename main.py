4db922f4b481214be5d615a0dcfbee82ac149e5a
Damian <damiankwiecien09@gmail.com>
2025-11-28 13:48:44 +0000

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 def on_button_pressed_a():
-    global IsOverrideTrue
-    IsOverrideTrue = not (IsOverrideTrue)
-    if IsOverrideTrue:
+    global IsOverride, LedLightStatus
+    IsOverride = not (IsOverride)
+    if IsOverride:
+        LedLightStatus = True
+        pins.digital_write_pin(DigitalPin.P0, 1)
+        basic.show_icon(IconNames.SQUARE)
+    elif LedLightStatus:
+        pins.digital_write_pin(DigitalPin.P0, 1)
         basic.show_icon(IconNames.SQUARE)
     else:
+        pins.digital_write_pin(DigitalPin.P0, 0)
         basic.show_leds("""
             # . # . #
             . # # # .
@@ -11,29 +17,25 @@ def on_button_pressed_a():
             . # # # .
             # . # . #
             """)
+    datalogger.log(datalogger.create_cv("Event", 0),
+        datalogger.create_cv("Value", IsOverride),
+        datalogger.create_cv("Light", input.light_level()),
+        datalogger.create_cv("Cycles", LightOnCount))
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    global LightOnThreshold
+    global LightOnThreshold, LedLightStatus
     LightOnThreshold = input.light_level()
-    basic.show_string("T")
-    basic.show_number(LightOnThreshold)
-    basic.pause(1000)
-    basic.show_leds("""
-        # . # . #
-        . # # # .
-        # # # # #
-        . # # # .
-        # . # . #
-        """)
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-def on_gesture_shake():
     basic.show_string("C")
-    basic.show_number(LightOnCount)
-    basic.pause(1000)
-    if LedLightStatus:
+    basic.show_number(LightOnThreshold)
+    basic.pause(500)
+    if input.light_level() < LightOnThreshold:
+        LedLightStatus = True
+        pins.digital_write_pin(DigitalPin.P0, 1)
         basic.show_icon(IconNames.SQUARE)
+    else:
+        LedLightStatus = False
+        pins.digital_write_pin(DigitalPin.P0, 0)
         basic.show_leds("""
             # . # . #
             . # # # .
@@ -41,35 +43,75 @@ def on_gesture_shake():
             . # # # .
             # . # . #
             """)
+    datalogger.log(datalogger.create_cv("Event", "Calibration"),
+        datalogger.create_cv("Value", LightOnThreshold),
+        datalogger.create_cv("Light", LightOnThreshold),
+        datalogger.create_cv("Cycles", LightOnCount))
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+# On shake gesture, shows (P) for power, shows the light on count, pauses for 700ms to avoid bugs, and shows either the sun or moon, depending if the led light status variable or the is override variable true
+
+def on_gesture_shake():
+    basic.show_string("P")
+    basic.show_number(LightOnCount)
+    basic.pause(700)
+    if LedLightStatus or IsOverride:
+        basic.show_icon(IconNames.SQUARE)
     else:
-        pass
+        basic.show_leds("""
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            """)
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
-LedLightStatus = False
+# Sets the value at which the light will turn on at (from 0-255) then sets the data logging columns
+loop_counter = 0
+current = 0
 LightOnCount = 0
-IsOverrideTrue = False
+LedLightStatus = False
+IsOverride = False
 LightOnThreshold = 0
-LightOnThreshold = 80
+LightOnThreshold = 110
+datalogger.set_column_titles("Event", "Light", "Light")
 
 def on_forever():
-    global LedLightStatus, LightOnCount
-    if not (IsOverrideTrue):
-        if input.light_level() < LightOnThreshold and not (LedLightStatus):
+    global current, LedLightStatus, LightOnCount, loop_counter
+    current = input.light_level()
+    if not (IsOverride):
+        if current < LightOnThreshold and not (LedLightStatus):
             LedLightStatus = True
             LightOnCount += 1
-        elif input.light_level() > LightOnThreshold + 10 and LedLightStatus:
+            datalogger.log(datalogger.create_cv("Event", "AutoOn"),
+                datalogger.create_cv("Value", 1),
+                datalogger.create_cv("Light", current),
+                datalogger.create_cv("Cycles", LightOnCount))
+        elif current > LightOnThreshold + 10 and LedLightStatus:
             LedLightStatus = False
-        if LedLightStatus or IsOverrideTrue:
-            basic.show_icon(IconNames.SQUARE)
-            pins.digital_write_pin(DigitalPin.P0, 1)
-        else:
-            pins.digital_write_pin(DigitalPin.P0, 0)
-            basic.show_leds("""
-                # . # . #
-                . # # # .
-                # # # # #
-                . # # # .
-                # . # . #
-                """)
-        basic.pause(100)
+            datalogger.log(datalogger.create_cv("Event", "AutoOff"),
+                datalogger.create_cv("Value", 0),
+                datalogger.create_cv("Light", current),
+                datalogger.create_cv("Cycles", LightOnCount))
+    if LedLightStatus or IsOverride:
+        pins.digital_write_pin(DigitalPin.P0, 1)
+        basic.show_icon(IconNames.SQUARE)
+    else:
+        pins.digital_write_pin(DigitalPin.P0, 0)
+        basic.show_leds("""
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            """)
+    loop_counter += 1
+    if loop_counter >= 5:
+        datalogger.log(datalogger.create_cv("Event", "Ambient"),
+            datalogger.create_cv("Value", current),
+            datalogger.create_cv("Light", current),
+            datalogger.create_cv("Cycles", LightOnCount))
+        loop_counter = 0
+    basic.pause(150)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-    IsOverrideTrue = !(IsOverrideTrue)
-    if (IsOverrideTrue) {
+    IsOverride = !(IsOverride)
+    if (IsOverride) {
+        LedLightStatus = true
+        basic.showIcon(IconNames.Square)
+    } else if (LedLightStatus) {
         basic.showIcon(IconNames.Square)
     } else {
         basic.showLeds(`
@@ -11,26 +14,22 @@ input.onButtonPressed(Button.A, function () {
             # . # . #
             `)
     }
+    datalogger.log(
+    datalogger.createCV("Event", 0),
+    datalogger.createCV("Value", IsOverride),
+    datalogger.createCV("Light", input.lightLevel()),
+    datalogger.createCV("Cycles", LightOnCount)
+    )
 })
 input.onButtonPressed(Button.B, function () {
     LightOnThreshold = input.lightLevel()
-    basic.showString("T")
-    basic.showNumber(LightOnThreshold)
-    basic.pause(1000)
-    basic.showLeds(`
-        # . # . #
-        . # # # .
-        # # # # #
-        . # # # .
-        # . # . #
-        `)
-})
-input.onGesture(Gesture.Shake, function () {
     basic.showString("C")
-    basic.showNumber(LightOnCount)
-    basic.pause(1000)
-    if (LedLightStatus) {
+    basic.showNumber(LightOnThreshold)
+    basic.pause(500)
+    if (input.lightLevel() < LightOnThreshold) {
+        LedLightStatus = true
         basic.showIcon(IconNames.Square)
+    } else {
         basic.showLeds(`
             # . # . #
             . # # # .
@@ -38,36 +37,87 @@ input.onGesture(Gesture.Shake, function () {
             . # # # .
             # . # . #
             `)
+        LedLightStatus = false
+    }
+    datalogger.log(
+    datalogger.createCV("Event", 0),
+    datalogger.createCV("Value", LightOnThreshold),
+    datalogger.createCV("Light", LightOnThreshold),
+    datalogger.createCV("Cycles", LightOnCount)
+    )
+})
+// On shake gesture, shows (P) for power, shows the light on count, pauses for 700ms to avoid bugs, and shows either the sun or moon, depending if the led light status variable or the is override variable true
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("P")
+    basic.showNumber(LightOnCount)
+    basic.pause(700)
+    if (LedLightStatus || IsOverride) {
+        basic.showIcon(IconNames.Square)
     } else {
-    	
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
     }
 })
-let LedLightStatus = false
+// Sets the value at which the light will turn on at (from 0-255) then sets the data logging columns
+let loop_counter = 0
+let current = 0
 let LightOnCount = 0
-let IsOverrideTrue = false
+let LedLightStatus = false
+let IsOverride = false
 let LightOnThreshold = 0
-LightOnThreshold = 80
+LightOnThreshold = 110
+datalogger.setColumnTitles(
+"Event",
+"Light",
+"Light"
+)
 basic.forever(function () {
-    if (!(IsOverrideTrue)) {
-        if (input.lightLevel() < LightOnThreshold && !(LedLightStatus)) {
+    current = input.lightLevel()
+    if (!(IsOverride)) {
+        if (current < LightOnThreshold && !(LedLightStatus)) {
             LedLightStatus = true
             LightOnCount += 1
-        } else if (input.lightLevel() > LightOnThreshold + 10 && LedLightStatus) {
+            datalogger.log(
+            datalogger.createCV("Event", "AutoOn"),
+            datalogger.createCV("Value", 1),
+            datalogger.createCV("Light", current),
+            datalogger.createCV("Cycles", LightOnCount)
+            )
+        } else if (current > LightOnThreshold + 10 && LedLightStatus) {
             LedLightStatus = false
+            datalogger.log(
+            datalogger.createCV("Event", "AutoOff"),
+            datalogger.createCV("Value", 0),
+            datalogger.createCV("Light", current),
+            datalogger.createCV("Cycles", LightOnCount)
+            )
         }
-        if (LedLightStatus || IsOverrideTrue) {
-            basic.showIcon(IconNames.Square)
-            pins.digitalWritePin(DigitalPin.P0, 1)
-        } else {
-            pins.digitalWritePin(DigitalPin.P0, 0)
-            basic.showLeds(`
-                # . # . #
-                . # # # .
-                # # # # #
-                . # # # .
-                # . # . #
-                `)
-        }
-        basic.pause(100)
     }
+    if (LedLightStatus || IsOverride) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    }
+    loop_counter += 1
+    if (loop_counter >= 5) {
+        datalogger.log(
+        datalogger.createCV("Event", "Ambient"),
+        datalogger.createCV("Value", current),
+        datalogger.createCV("Light", current),
+        datalogger.createCV("Cycles", LightOnCount)
+        )
+        loop_counter = 0
+    }
+    basic.pause(150)
 })
